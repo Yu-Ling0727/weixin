23dfb461d076c9750cd7e4aa9ce4bf9e2581baf6
Yu-Ling0727 <2757250790@qq.com>
2026-05-22 19:08:32 +0800

    Add files via upload

--- 22.py
+++ 22.py
@@ -1,23 +1,17 @@
-import tkinter as tk
-from tkinter import messagebox
-from tkinter import simpledialog
+import easygui as sg
 
-# 创建主窗口
-root = tk.Tk()
-root.withdraw()  # 隐藏主窗口
+# 信息弹窗
+sg.msgbox("这是一个提示消息")
 
-# 弹出消息提示框
-messagebox.showinfo("提示", "这是一个弹窗消息")
-
-# 弹出确认框（返回 True 或 False）
-result = messagebox.askyesno("确认", "你确定要继续吗？")
-print("用户选择：", result)
-
-# 弹出输入框，获取用户输入
-user_input = simpledialog.askstring("输入", "请输入你的名字：")
-if user_input == "王楦博":
-    print(f"{user_input},你好，傻逼")
+# 确认弹窗
+if sg.ynbox("是否继续？", choices=["是", "否"]):
+    print("用户点击是")
 else:
-    print(f'你好，{user_input}')
-# 关闭窗口
-root.destroy()
+    print("用户点击否")
+
+# 输入弹窗
+name = sg.enterbox("请输入你的名字")
+if name == "王楦博":
+    print(f"{name},你好，傻逼")
+else:
+    print(f'你好,{name}')
